2c887f1ac193767c72df0b429b2720e632b1f648
Binh Tran <mynameisbinh@gmail.com>
2013-02-20 19:11:51 -0800

    Mess with MXBeanTypes

--- src/playground/PlayGround.java
+++ src/playground/PlayGround.java
@@ -4,10 +4,12 @@
  */
 package playground;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.management.openmbean.CompositeData;
 
 /**
  *
@@ -19,16 +21,28 @@ public class PlayGround {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception{
-        FileWriter fw = new FileWriter("/home/ankel/file1.txt", true);
-        BufferedWriter w = new BufferedWriter(fw);
-        w.write("Hello");
-        w.newLine();
-        w.close();
-        w = new BufferedWriter(fw);
-        w.write("World");
-        w.newLine();
-        w.close();
-        Runtime.getRuntime().exec("cat ~/file1.txt").waitFor();
-        Runtime.getRuntime().exec("rm ~/file1.txt").waitFor();
+        ArrayList arr = new ArrayList();
+        for (int i = 0; i < 10000000; ++i) {
+            arr.add(i);
+        }
+        arr.clear();
+        for (int i = 0; i < 10000000; ++i) {
+            arr.add(10000000 - i);
+        }
+        arr.clear();
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+//        for (ObjectInstance bean : mbs.queryMBeans(null, null)) {
+//            System.out.println(bean.getObjectName().getCanonicalName());
+//        }
+        ObjectName mS = new ObjectName("java.lang:name=PS MarkSweep,type=GarbageCollector");
+//        for (MBeanAttributeInfo ai : mbs.getMBeanInfo(mS).getAttributes()) {
+//            System.out.println(ai.getName());
+//        }
+        
+        CompositeData c1 = (CompositeData) mbs.getAttribute(mS, "LastGcInfo");
+//        for (String s : c1.getCompositeType().keySet()) {
+//            System.out.println(s);
+//        }
+        System.out.println(c1.get("memoryUsageAfterGc").getClass());
     }
 }
